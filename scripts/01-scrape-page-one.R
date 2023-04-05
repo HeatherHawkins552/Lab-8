4948af248c0d83e324520db048c4eaa05392a726
Heather Hawkins <hawkhc21@wfu.edu>
2023-04-04 22:45:51 -0400

    #4

--- scripts/01-scrape-page-one.R
+++ scripts/01-scrape-page-one.R
@@ -32,23 +32,47 @@ links <- page %>%
 artists <- page %>%
   html_nodes(".iteminfo") %>%
   html_node(".artist") %>%
-  ___
+  html_attr("title")
 
 # put together in a data frame -------------------------------------------------
 
-first_ten <- tibble(
-  title = ___,
-  artist = ___,
-  link = ___
-)
+uoe_art <- tibble(
+  title = titles,
+  artist = artists,
+  link = links)
+
+uoe_art
+
 
 # scrape second ten paintings --------------------------------------------------
 
-second_url <- "___"
+second_url <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset=10"
 
-page <- read_html(second_url)
-...
+page2 <- read_html(second_url)
+
+
+titles2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_text() %>%
+  str_squish()
+
+links2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_attr("href") %>%
+  str_replace(".", "https://collections.ed.ac.uk/art/")
+
+artists2 <- page2 %>%
+  html_nodes(".iteminfo") %>%
+  html_node(".artist") %>%
+  html_text() %>%
+  str_squish()
 
 second_ten <- tibble(
-  ...
-)
+  title = titles2,
+  artist = artists2,
+  link = links2)
+
+second_ten
+
